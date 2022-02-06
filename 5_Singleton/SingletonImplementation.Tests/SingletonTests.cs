ed312993876ddd594894fa11813ca32d820930ae
Grzegorz Waluś <gwalus1404@gmail.com>
2022-02-06 15:49:50 +0100

    5_Singleton_v2

--- 5_Singleton/SingletonImplementation.Tests/SingletonTests.cs
+++ 5_Singleton/SingletonImplementation.Tests/SingletonTests.cs
@@ -1,3 +1,4 @@
+using Autofac;
 using Interfaces;
 using NUnit.Framework;
 
@@ -24,6 +25,30 @@ namespace SingletonImplementation.Tests
             int tp = rf.GetTotalPopulation(names);
 
             Assert.That(tp, Is.EqualTo(17500000 + 17400000));
+        }
+
+        [Test]
+        public void ConfigurablePopulationTest()
+        {
+            var rf = new ConfigurableRecordFinder(new DummyDatabase());
+            var names = new[] { "alpha", "gamma" };
+            int tp = rf.GetTotalPopulation(names);
+
+            Assert.That(tp, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void DIPopulationTest()
+        {
+            var cb = new ContainerBuilder();
+            cb.RegisterType<OrdinaryDatabase>()
+                .As<IDatabase>()
+                .SingleInstance();
+            cb.RegisterType<ConfigurableRecordFinder>();
+
+            using var c = cb.Build();
+            
+            var rf = c.Resolve<ConfigurableRecordFinder>();
         }
     }
 }
--- 5_Singleton/SingletonImplementation/Interfaces/IDatabase.cs
+++ 5_Singleton/SingletonImplementation/Interfaces/IDatabase.cs
@@ -18,6 +18,61 @@ namespace Interfaces
         }
     }
 
+    public class ConfigurableRecordFinder
+    {
+        private readonly IDatabase _database;
+
+        public ConfigurableRecordFinder(IDatabase database)
+        {
+            _database = database;
+        }
+
+        public int GetTotalPopulation(IEnumerable<string> names)
+        {
+            int result = 0;
+            foreach (string name in names)
+                result += _database.GetPopulation(name);
+            return result;
+        }
+    }
+
+    public class OrdinaryDatabase : IDatabase
+    {
+        private Dictionary<string, int> _capitals;
+
+        private OrdinaryDatabase()
+        {
+            _capitals = File.ReadAllLines(
+                //"capitals.txt"
+                Path.Combine(new FileInfo(typeof(IDatabase).Assembly.Location).DirectoryName,
+                "capitals.txt")
+                )
+                .Batch(2)
+                .ToDictionary(
+                    list => list.ElementAt(0).Trim(),
+                    list => int.Parse(list.ElementAt(1))
+                );
+        }
+        public int GetPopulation(string name)
+        {
+            return _capitals[name];
+        }
+    }
+
+    public class DummyDatabase : IDatabase
+    {
+        public int GetPopulation(string name)
+        {
+            return new Dictionary<string, int>
+            {
+                ["alpha"] = 1,
+                ["beta"] = 2,
+                ["gamma"] = 3,
+            }[name];
+        }
+    }
+
+
     public class SingletonDatabase : IDatabase
     {
         private Dictionary<string, int> _capitals;
